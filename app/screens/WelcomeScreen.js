9f0a624283e34d05e45cef0a30c84bed68abc452
Evelyn Graetz <egraetz@Evelyns-MacBook-Pro.local>
2021-03-07 16:28:55 +0100

    implement recipe data array and make home screen dynamic

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -3,55 +3,39 @@ import { ImageBackground, StyleSheet } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
 
+const data = [
+  {
+    recipeName: "Belugalinsen Curry",
+    thumbnail: "https://picsum.photos/id/292/200/300",
+    duration: 10,
+  },
+  {
+    recipeName: "Zucchini-Röllchen mit Cashew-Ricotta",
+    thumbnail: "https://picsum.photos/id/225/200/300",
+    duration: 20,
+  },
+  {
+    recipeName: "Die echte Sauce Bolognese",
+    thumbnail: "https://picsum.photos/id/102/200/300",
+    duration: 100,
+  },
+];
+
 const WelcomeScreen = ({ navigation }) => (
   <ScrollView>
-    <Card>
-      <Card.Title title="Belugalinsen Curry" />
-      <Card.Cover source={{ uri: "https://picsum.photos/id/292/200/300" }} />
-      <Card.Actions>
-        <Button
-          onPress={() =>
-            navigation.navigate("RecipeDetailScreen", {
-              recipeName: "Belugalinsen Curry",
-              thumbnail: "https://picsum.photos/id/292/200/300",
-              duration: 10,
-            })
-          }
-        >
-          Open
-        </Button>
-      </Card.Actions>
-      <Card.Title title="Zucchini-Röllchen mit Cashew-Ricotta" />
-      <Card.Cover source={{ uri: "https://picsum.photos/id/225/200/300" }} />
-      <Card.Actions>
-        <Button
-          onPress={() =>
-            navigation.navigate("RecipeDetailScreen", {
-              recipeName: "Zucchini-Röllchen mit Cashew-Ricotta",
-              thumbnail: "https://picsum.photos/id/225/200/300",
-              duration: 20,
-            })
-          }
-        >
-          Open
-        </Button>
-      </Card.Actions>
-      <Card.Title title="Die echte Sauce Bolognese" />
-      <Card.Cover source={{ uri: "https://picsum.photos/id/102/200/300" }} />
-      <Card.Actions>
-        <Button
-          onPress={() =>
-            navigation.navigate("RecipeDetailScreen", {
-              recipeName: "Die echte Sauce Bolognese",
-              thumbnail: "https://picsum.photos/id/102/200/300",
-              duration: 100,
-            })
-          }
-        >
-          Open
-        </Button>
-      </Card.Actions>
-    </Card>
+    {data.map((recipe) => (
+      <Card>
+        <Card.Title title={recipe.recipeName} />
+        <Card.Cover source={{ uri: recipe.thumbnail }} />
+        <Card.Actions>
+          <Button
+            onPress={() => navigation.navigate("RecipeDetailScreen", recipe)}
+          >
+            Open
+          </Button>
+        </Card.Actions>
+      </Card>
+    ))}
   </ScrollView>
 );
 
