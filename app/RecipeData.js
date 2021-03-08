2ee9a3f35546eda24ac924a991a1491ca2c03b1a
Evelyn Graetz <evelyn.graetz@gmx.de>
2021-03-08 16:18:52 +0100

    separate data struct from view

---
+++ app/RecipeData.js
@@ -0,0 +1,37 @@
+export const data = [
+  {
+    recipeName: "Belugalinsen Curry",
+    thumbnail: "https://picsum.photos/id/292/200/300",
+    duration: 10,
+  },
+  {
+    recipeName: "Zucchini-Röllchen mit Cashew-Ricotta",
+    thumbnail: "https://picsum.photos/id/225/200/300",
+    duration: 20,
+  },
+  {
+    recipeName: "Die echte Sauce Bolognese",
+    thumbnail: "https://picsum.photos/id/102/200/300",
+    duration: 100,
+  },
+  {
+    recipeName: "Bircher Müsli",
+    thumbnail: "https://picsum.photos/id/312/200/300",
+    duration: 10,
+    ingredients: [
+      "100g Frucht",
+      "20g Nüsse",
+      "50g Haferflocken",
+      "20g Rosinen",
+      "200ml Pflanzendrink",
+      "1 Prise Salz",
+      "1/2 TL Zimt",
+    ],
+    steps: [
+      "1. Frucht in kleine Stückchen schneiden. Nüsse grob hacken.",
+      "2. Beides in eine Schale geben. Mit Haferflocken, Rosinen und dem Pflanzendrink mischen.",
+      "3. Salz und Zimt hinzugeben.",
+      "4. Für mind. 3h in den Kühlschrank (besser über Nacht).",
+    ],
+  },
+];
