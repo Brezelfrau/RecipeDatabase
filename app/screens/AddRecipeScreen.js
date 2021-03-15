c85811b19383819ae57f6da0e43b269ecd61b07d
Evelyn Graetz <evelyn.graetz@gmx.de>
2021-03-15 17:48:43 +0100

    implement form for AddRecipeScreen

--- app/screens/AddRecipeScreen.js
+++ app/screens/AddRecipeScreen.js
@@ -1,10 +1,19 @@
 import * as React from "react";
 import { ScrollView } from "react-native-gesture-handler";
 import { Text } from "react-native-paper";
+import t from "tcomb-form-native";
+
+const Form = t.form.Form;
+
+const RecipeForm = t.struct({
+  title: t.String,
+  duration: t.Number,
+});
 
 const AddRecipeScreen = ({ navigation }) => (
   <ScrollView>
     <Text>hello</Text>
+    <Form type={RecipeForm} />
   </ScrollView>
 );
 
